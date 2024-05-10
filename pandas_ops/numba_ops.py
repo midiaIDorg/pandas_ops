2d4acccf6e99e1771a86a1f68b042c0681b059a4
matteo <matteo.lacki@gmail.com>
2024-05-10 13:38:45 +0200

    made available both _ with numpy or without with pandas.

--- pandas_ops/numba_ops.py
+++ pandas_ops/numba_ops.py
@@ -9,7 +9,8 @@ def inputs_series_to_numpy(foo):
     def wrapper(*args, **kwargs):
         args = [arg.to_numpy() if isinstance(arg, pd.Series) else arg for arg in args]
         kwargs = {
-            k: np.array(v) if isinstance(v, pd.Series) else v for k, v in kwargs.items()
+            k: v.to_numpy() if isinstance(v, pd.Series) else v
+            for k, v in kwargs.items()
         }
         return foo(*args, **kwargs)
 
