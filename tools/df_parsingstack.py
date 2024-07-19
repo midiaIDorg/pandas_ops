5ccda2f3ecb4b1649fb6cc5ca8470d36e584f9a4
matteo <matteo.lacki@gmail.com>
2024-07-19 17:08:53 +0200

    rolled back to CLI+toml with sql

--- tools/df_parsingstack.py
+++ tools/df_parsingstack.py
@@ -13,54 +13,59 @@ parser = argparse.ArgumentParser(
     "Combine multiple table files into one and add in meta info by regex-based parsing the submitted paths.",
     formatter_class=argparse.ArgumentDefaultsHelpFormatter,
 )
-parser.add_argument(
-    "config",
-    help="How to parse submitted paths into a table.",
-    default="",
-    type=str,
-)
 parser.add_argument("output", help="Path to the output table.", type=pathlib.Path)
 parser.add_argument(
     "inputs", help="Path to the input table.", type=pathlib.Path, nargs="+"
 )
 
-# parser.add_argument(
-#     "--verbose",
-#     help="Be more verbose.",
-#     action="store_true",
-# )
-# parser.add_argument(
-#     "--paths_regex",
-#     help="How to parse submitted paths into a table.",
-#     default="",
-#     type=str,
-# )
-
-# parser.add_argument(
-#     "--howtostack",
-#     help="How to stack the tables?",
-#     choices=["vertical", "horizontal"],
-#     default="vertical",
-# )
-# parser.add_argument(
-#     "--partition_cols",
-#     nargs="+",
-#     help="Which columns should be used to split the parquet file?",
-#     type=str,
-# )
+parser.add_argument(
+    "--verbose",
+    help="Be more verbose.",
+    action="store_true",
+)
+parser.add_argument(
+    "--paths_regex",
+    help="How to parse submitted paths into a table.",
+    default="",
+    type=str,
+)
+parser.add_argument(
+    "--howtostack",
+    help="How to stack the tables?",
+    choices=["vertical", "horizontal"],
+    default="vertical",
+)
+parser.add_argument(
+    "--partition_cols",
+    nargs="+",
+    help="Which columns should be used to split the parquet file?",
+    type=str,
+    default=None,
+)
+parser.add_argument(
+    "--sql",
+    help="How to parse submitted paths into a table.",
+    default={},
+    type=str,
+)
 
 args = parser.parse_args().__dict__
 
 if __name__ == "__main__":
     dfs = []
-    duckcon = duckdb.connect()
-    with open(args["config"], "rb") as f:
-        config = tomllib.load(f)
-        _verbose = config.get("verbose", False)
-        if _verbose:
-            pprint(config)
+    _verbose = args["verbose"]
 
-    path_pattern = re.compile(config["paths_regex"])
+    df_filter = lambda df: df
+    if args["sql"]:
+        duckcon = duckdb.connect()
+        with open(args["sql"], "rb") as f:
+            config = tomllib.load(f)
+            if _verbose:
+                pprint(config)
+        if "sql" in config:
+            df_filter = lambda df: duckdb.query(config["sql"]).df()
+
+    path_pattern = re.compile(args["paths_regex"])
 
     for path in args["inputs"]:
         if _verbose:
@@ -70,8 +75,7 @@ if __name__ == "__main__":
                 print(f"Missing `{path}`")
         else:
             df = read_df(path)
-            if "datawise_filter_sql" in config:
-                df = duckcon.query(config["datawise_filter_sql"]).df()
+            df = df_filter(df)
             match = path_pattern.search(str(path))
             if match:
                 meta = pd.DataFrame([match.groupdict()] * len(df))
@@ -89,7 +93,7 @@ if __name__ == "__main__":
             out,
             args["output"],
             index=False,
-            **config.get("save_kwargs", {}),
+            partition_cols=args["partition_cols"],
         )
 
 # TODO: test it under csvs using different paths.
