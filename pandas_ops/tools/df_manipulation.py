a2971b4fdad04a3e5e78517a8774a980eaac7304
matteo <matteo.lacki@gmail.com>
2024-11-11 11:02:34 +0100

    added docs

--- pandas_ops/tools/df_manipulation.py
+++ pandas_ops/tools/df_manipulation.py
@@ -9,11 +9,15 @@ from pandas_ops.parsers.misc import parse_key_equal_value
 
 @click.command(context_settings={"show_default": True})
 @click.argument("output_path", type=Path, nargs=1)
-@click.argument("input_path", type=Path, nargs=-1)
-def df_concat(output_path: Path, input_path: list[Path]) -> None:
+@click.argument("input_paths", type=Path, nargs=-1)
+def df_concat(output_path: Path, input_paths: list[Path]) -> None:
     """
     Concatenate tables.
+
+    Arguments:\n
+        output_path (pathlib.Path): Where to store outputs.\n
+        input_path (list[pathlib.Path]): Which tables to concatenate.
     """
-    in_dfs = (read_df(in_path) for in_path in input_path)
+    in_dfs = (read_df(in_path) for in_path in input_paths)
     out_df = pd.concat(in_dfs, axis=1)
     save_df(out_df, output_path)
