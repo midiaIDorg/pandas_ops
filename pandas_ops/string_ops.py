09753c14811fe7bafbbf6e5cd5ead8e6cff066f2
Mateusz Lacki <matteo.lacki@gmail.com>
2025-02-18 13:37:09 +0100

    added string_ops.py: there support for iterating over glob expressions with stars.

---
+++ pandas_ops/string_ops.py
@@ -0,0 +1,30 @@
+import re
+
+from itertools import product
+
+from pathlib import Path
+
+
+def iter_glob(
+    path: str | Path,
+    _brackets_pattern: re.Pattern = re.compile(r"\{(.*?)\}"),
+    _sub_bracket_sign: str = "{}",
+    _split_sign: str = ",",
+):
+    bracket_occurences = [o.split(_split_sign) for o in _brackets_pattern.findall(path)]
+    clean_text = _brackets_pattern.sub(_sub_bracket_sign, path)
+    for fills in product(*bracket_occurences):
+        yield clean_text.format(*fills)
+
+
+def test_iter_glob():
+    path = "stats/**/*.{csv,json}_{1,2,4}"
+    expected_outcome = [
+        "stats/**/*.csv_1",
+        "stats/**/*.csv_2",
+        "stats/**/*.csv_4",
+        "stats/**/*.json_1",
+        "stats/**/*.json_2",
+        "stats/**/*.json_4",
+    ]
+    assert list(iter_glob(path)) == expected_outcome
