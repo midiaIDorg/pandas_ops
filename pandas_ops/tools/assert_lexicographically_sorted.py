29080720f75d760a4c23ac3def7b0a973b76e41e
Mateusz Lacki <matteo.lacki@gmail.com>
2025-03-25 19:54:12 +0100

    none case

--- pandas_ops/tools/assert_lexicographically_sorted.py
+++ pandas_ops/tools/assert_lexicographically_sorted.py
@@ -7,10 +7,23 @@ from pathlib import Path
 
 @click.command(context_settings={"show_default": True})
 @click.argument("input_path", type=Path)
-@click.argument("columns", type=Path, nargs=-1)
+@click.argument("columns", nargs=-1)
 @click.option("--strictly", is_flag=True)
 def assert_lexicographically_sorted(
     input_path: Path, columns, strictly: bool = False
 ) -> None:
+    """Check a table is lexicographically sorted w.r.t. the provided columns.\n
+
+    Arguments:\n
+        input_path (Path): Path to a table.
+        columns (list[str]): A sequence of strings describing column names.
+    """
+    if "None.d" in str(input_path):
+        print("None.d is trivially lexicographically sorted.")
+        return None
+
     data = read_df(input_path, columns=columns)
-    assert is_sorted_lexicographically(strictly, *[data[col] for col in columns])
+    assert is_sorted_lexicographically(
+        strictly, *[data[col] for col in columns]
+    ), f"`{input_path}` is not strictly lexicographically sorted by {columns}."
+    print(f"`{input_path}` is strictly lexicographically sorted by {columns}.")
